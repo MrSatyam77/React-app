5f7981130979152b7435bc593ad4e2542d593065
Satyam Jasoliya <satyam.jasoliya@i-link.co.in>
2021-02-25 18:59:23 +0530

    first commit

--- src/axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-const instance = axios.create({
-    baseURL:'https://react-my-burger-d767f-default-rtdb.firebaseio.com/'
-});
-instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
-instance.defaults.headers.post['Content-Type'] = 'application/json';
-export default instance;
--- src/components/UI/Spinner/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Spinner.css';
-const spinner = () =>{
-    return(
-        <div className="Loader">Loading...</div>
-    );
-}
-export default spinner; 
